3a630dba09b73d0094fb01bb0a9d34e0931ae70c
BeijingDev <beijingq8@gmail.com>
2025-11-28 18:44:33 +0700

    update download app url

--- logic.js
+++ logic.js
@@ -1,12 +1,14 @@
 
-  document.getElementsByClassName("download-app")[0].addEventListener("click", function(e) {
-    e.preventDefault();
-    let userAgent = navigator.userAgent || navigator.vendor || window.opera;
-    if (/android/i.test(userAgent)) {
-      window.location.href = "https://images.1097638.com/wsd-images-prod/rr88f4/merchant_resource/android/rr88f4_2.4.67_20250425190143.apk";
-    } else if (/iPhone|iPad|iPod/i.test(userAgent)) {
-      window.location.href = "https://images.1097638.com/wsd-images-prod/rr88f4/merchant_resource/mobileconfig/rr88f4_2.4.3_20250425190122.mobileconfig";
-    } else {
-      alert("Vui lòng tải ứng dụng trên điện thoại Android hoặc iOS.");
-    }
-  });
+document.getElementsByClassName("download-app")[0].addEventListener("click", function (e) {
+  e.preventDefault();
+  let userAgent = navigator.userAgent || navigator.vendor || window.opera;
+  if (/android/i.test(userAgent)) {
+    //window.location.href = "https://images.1097638.com/wsd-images-prod/rr88f4/merchant_resource/android/rr88f4_2.4.67_20250425190143.apk";
+    window.location.href = "https://rr88-download-app.pages.dev";
+  } else if (/iPhone|iPad|iPod/i.test(userAgent)) {
+    //window.location.href = "https://images.1097638.com/wsd-images-prod/rr88f4/merchant_resource/mobileconfig/rr88f4_2.4.3_20250425190122.mobileconfig";
+    window.location.href = "https://rr88-download-app.pages.dev";
+  } else {
+    alert("Vui lòng tải ứng dụng trên điện thoại Android hoặc iOS.");
+  }
+});
